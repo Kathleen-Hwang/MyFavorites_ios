21219e8716b47c66bef88c01df6f9c8c003535e3
Mac <Mac@macui-imac-6.local>
2016-09-20 14:47:41 +0900

    Initial Commit

---
+++ MyFavorites/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyFavorites
+//
+//  Created by Mac on 2016. 9. 20..
+//  Copyright © 2016년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
